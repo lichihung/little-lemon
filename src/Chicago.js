e7594045e8ec1a7a08a42411c2021ca765df37af
lichihung <lichihun@umich.edu>
2024-03-07 22:26:59 -0800

    testimonials

--- src/Chicago.js
+++ src/Chicago.js
@@ -1,8 +1,18 @@
+import owners_A from "../src/assets/Mario_and_Adrian_A.jpg";
+import owners_B from "../src/assets/Mario_and_Adrian_B.jpg";
 function Chicago(){
     return(
-        <>
-        <h1>Little Lemon Chicago</h1>
-        </>
+        <div className="about-section">
+            <p className="title yellow">Little Lemon</p>
+            <p className="subtitle green">Chicago</p>
+            <p className="paragraph black">Based in Chicago, Illinois, Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day.</p>
+            <br/>
+            <p className="paragraph black">Little Lemon is owned by two Italian brothers, Mario and Adrian, who moved to the United States to pursue their shared dream of owning a restaurant. To craft the menu, Mario relies on family recipes and his experience as a chef in Italy. Adrian does all the marketing for the restaurant and led the effort to expand the menu beyond classic Italian to incorporate additional cuisines from the Mediterranean region.</p>
+            <div className="about-img-container">
+                <img src={owners_A} alt="Mario and Adrian" class="about-img"/>
+                <img src={owners_B} alt="Mario and Adrian" class="about-img"/>
+            </div>
+        </div>
     )
 };
 
--- src/CustomersSay.js
+++ src/CustomersSay.js
@@ -1,8 +1,72 @@
+import pasta from "../src/assets/pasta.jpg";
+import fish from "../src/assets/fish.jpg";
+import greeksalad from "../src/assets/greeksalad.jpg";
+import lemondessert from "../src/assets/lemondessert.jpg";
 function CustomersSay(){
     return(
-        <>
-        <h1>Testimonials</h1>
-        </>
+        <div className="testimonials-section">
+            <section className="testimonials-title title black">Testimonials</section>
+            <section className="testimonials-box">
+                <article >
+                    <div className="testimonials-content">
+                    <div className="rating">
+                        <i className="fa-solid fa-star yellow"></i>
+                        <i className="fa-solid fa-star yellow"></i>
+                        <i className="fa-solid fa-star yellow"></i>
+                        <i className="fa-solid fa-star yellow"></i>
+                        <i className="fa-regular fa-star yellow"></i>
+                    </div>
+                    <div className="img-container">
+                        <img src={pasta} alt="Pasta" class="review-img"/>
+                    </div>
+                    <p className="paragraph black review">“The pasta was a delectable blend of rich flavors. The best pasta I've had in years!”</p>
+                    <p className="card-title black profile">- Peter J.</p>
+                    </div>
+                </article>
+                <article className="testimonials-content">
+                    <div className="rating">
+                        <i className="fa-solid fa-star yellow"></i>
+                        <i className="fa-solid fa-star yellow"></i>
+                        <i className="fa-solid fa-star yellow"></i>
+                        <i className="fa-solid fa-star yellow"></i>
+                        <i className="fa-solid fa-star yellow"></i>
+                    </div>
+                    <div className="img-container">
+                        <img src={fish} alt="Fish" class="review-img"/>
+                    </div>
+                    <p className="paragraph black review">"The fish was a culinary masterpiece, with its delicate seasoning that elevated each bite."</p>
+                    <p className="card-title black profile">- Jane V.</p>
+                </article>
+                <article className="testimonials-content">
+                    <div className="rating">
+                        <i className="fa-solid fa-star yellow"></i>
+                        <i className="fa-solid fa-star yellow"></i>
+                        <i className="fa-solid fa-star yellow"></i>
+                        <i className="fa-solid fa-star yellow"></i>
+                        <i className="fa-solid fa-star yellow"></i>
+                    </div>
+                    <div className="img-container">
+                        <img src={lemondessert} alt="Lemon dessert" class="review-img"/>
+                    </div>
+                    <p className="paragraph black review">"The lemon cake is the best! It was refreshingly light yet indulgently satisfying."</p>
+                    <p className="card-title black profile">- Lucas R.</p>
+                </article>
+                <article className="testimonials-content">
+                    <div className="rating">
+                        <i className="fa-solid fa-star yellow"></i>
+                        <i className="fa-solid fa-star yellow"></i>
+                        <i className="fa-solid fa-star yellow"></i>
+                        <i className="fa-solid fa-star yellow"></i>
+                        <i className="fa-regular fa-star yellow"></i>
+                    </div>
+                    <div className="img-container">
+                        <img src={greeksalad} alt="Greek Salad" class="review-img"/>
+                    </div>
+                    <p className="paragraph black review">"The Greek salad was a refreshing medley that transported me straight to the Mediterranean."</p>
+                    <p className="card-title black profile">- Ruth C.</p>
+                </article>
+            </section>
+        </div>
     )
 };
 
